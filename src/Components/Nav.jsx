55889b145fb0aef4c35ccdbb09b8a275ead797f6
wormrider <126498815+wormrider@users.noreply.github.com>
2023-10-24 16:01:09 +0100

    task 7 votes on article done

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,12 +1,25 @@
 import {Link} from "react-router-dom"
+import React from 'react';
+import { useUserContext } from './UserContext';
 
 const Nav = () => {
-    
-    return (<nav>
+    const { user } = useUserContext();
+
+    return (<>
+    {user ? (
+        <nav>
+        <Link to="/">Home</Link>
+        <Link to="/articles/topic?topic="> Topics</Link>
+        <Link to="/user"> Profile</Link>
+        <Link to="/articles/add-article"> Add Article</Link>
+        </nav>)
+        :
+        (<nav>
         <Link to="/">Home</Link>
         <Link to="/articles/topic?topic="> Topics</Link>
         <Link to="/user/:user_id"> Profile</Link>
-        </nav>)
-}
+        </nav>)}
+    </>
+)}
 
 export default Nav;
